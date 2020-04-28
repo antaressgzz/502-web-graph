4b318dfd9a6f5a46a61bb379fdc53274ca01b1dc
antaressgzz <antaressgzz@gmail.com>
2020-04-28 01:41:17 +0000

    t

--- .ipynb_checkpoints/502_tlds-checkpoint.sh
+++ .ipynb_checkpoints/502_tlds-checkpoint.sh
@@ -3,5 +3,14 @@ hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
 -files tld_mapper.py \
 -input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00000.warc.wat.gz \
 -input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00001.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00002.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00003.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00004.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00005.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00006.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00007.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00008.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00009.warc.wat.gz \
+-input s3://commoncrawl/crawl-data/CC-MAIN-2020-10/segments/1581875141396.22/wat/CC-MAIN-20200216182139-20200216212139-00010.warc.wat.gz \
 -output test \
 -mapper tld_mapper.py
