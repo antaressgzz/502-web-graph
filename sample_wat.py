0189755bd6b83687ed5180bccbfd4bfa7b33b7bd
antaressgzz <antaressgzz@gmail.com>
2020-05-01 20:45:53 -0400

    random sample file

---
+++ sample_wat.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sys
+import os
+
+n = sys.argv[1]
+out = sys.argv[2]
+f = open('wat.paths')
+path_list = f.readlines()
+
+pre = 'hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \n-D mapreduce.job.reduces=0 \n-files tld_mapper.py \n'
+
+input_paths = ''.join(['-input s3://commoncrawl/'+path_list[i] for i in \
+                       list(np.random.choice(56000, int(n), replace=False))])
+end = '-output '+out+'\n-mapper tld_mapper.py'
+os.system(pre+input_paths+end)
